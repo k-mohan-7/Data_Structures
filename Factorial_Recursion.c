ca94d6846c8f425d1be9487e4130931be0039392
k-mohan-7 <120168677+k-mohan-7@users.noreply.github.com>
2023-08-25 11:56:47 +0530

    Create Factorial_Recursion.c

---
+++ Factorial_Recursion.c
@@ -0,0 +1,17 @@
+a#include<stdio.h>
+int fact(int n)
+{  if (n==0||n==1)
+   {
+    return 1;
+   }
+   else{
+     return n*fact(n-1);
+   }
+}
+int main()
+{
+    int n;
+    printf("enter the number");
+    scanf("%d",&n);
+    printf("%d is factorial of number",fact(n));
+}
